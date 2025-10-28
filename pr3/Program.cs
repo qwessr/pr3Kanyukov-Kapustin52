a22bb697b57569d102f320087caad88c3d38102c
alexeys6969 <alexshashin.2023@gmail.com>
2025-10-28 16:24:30 +0500

    Создана Program и Библиотека классов и класс Snake.Kanyukov&Kapustin

--- pr3/Program.cs
+++ pr3/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.Remoting.Messaging;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,6 +11,16 @@ namespace pr3
     {
         static void Main(string[] args)
         {
-        }
+            
+        public static List<Leaders> Leaders = new List<Leaders>();
+
+        public static List<ViewModelUserSettings> remoteIPAddress = new List<ViewModelUserSettings>();
+
+        public static List<ViewModelGames> viewModelGames = new List<ViewModelGames>();
+        
+        private static int localPort = 5001;
+       
+        public static int MaxSpeed = 15;
     }
 }
+
