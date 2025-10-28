d8267ecf29975a9fc5d0deccad85304dadc2de10
alexeys6969 <alexshashin.2023@gmail.com>
2025-10-28 16:50:03 +0500

    Метод AddSnake.Kanyuvko&Kapustin

--- pr3/Program.cs
+++ pr3/Program.cs
@@ -102,6 +102,22 @@ namespace pr3
                 Console.WriteLine("Возникло исключение: " + ex.ToString() + "\n  " + ex.Message);
             }
         }
+        public static int AddSnake()
+        {
+            ViewModelGames viewModelGamesPlayer = new ViewModelGames();
+            viewModelGamesPlayer.SnakesPlayers = new Snakes()
+            {
+                Points = new List<Snakes.Point>() {
+            new Snakes.Point() { X = 30, Y = 10 },
+            new Snakes.Point() { X = 20, Y = 10 },
+            new Snakes.Point() { X = 10, Y = 10 },
+        },
+                direction = Snakes.Direction.Start
+            };
+            viewModelGamesPlayer.Points = new Snakes.Point(new Random().Next(10, 783), new Random().Next(10, 410));
+            viewModelGames.Add(viewModelGamesPlayer);
+            return viewModelGames.FindIndex(x => x == viewModelGamesPlayer);
+        }
     }
 }
 
