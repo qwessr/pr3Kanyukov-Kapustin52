2dce518379eb9e6663ec21ec37abe42edb6a3dfd
alexeys6969 <alexshashin.2023@gmail.com>
2025-10-28 16:30:12 +0500

    Класс ViewModelUserSettings.Kanyukov&Kapustin

---
+++ Common/ViewModelUserSettings.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Common
+{
+    public class ViewModelUserSettings
+    {
+        public string IPAddress { get; set; }
+        public string Port { get; set; }
+        public string Name { get; set; }
+        public int IdSnake = -1;
+    }
+}
