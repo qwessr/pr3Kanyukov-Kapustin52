2942b5167a2f247461d4e003e554ee6ff7ec16b0
alexeys6969 <alexshashin.2023@gmail.com>
2025-10-28 16:34:13 +0500

    Подключение класса Common k Snake.Kanyukov&Kapustin

--- pr3/Program.cs
+++ pr3/Program.cs
@@ -4,23 +4,25 @@ using System.Linq;
 using System.Runtime.Remoting.Messaging;
 using System.Text;
 using System.Threading.Tasks;
+using Common;
 
 namespace pr3
 {
     internal class Program
     {
-        static void Main(string[] args)
-        {
-            
         public static List<Leaders> Leaders = new List<Leaders>();
 
         public static List<ViewModelUserSettings> remoteIPAddress = new List<ViewModelUserSettings>();
 
         public static List<ViewModelGames> viewModelGames = new List<ViewModelGames>();
-        
+
         private static int localPort = 5001;
-       
+
         public static int MaxSpeed = 15;
+        static void Main(string[] args)
+        {
+
+        }
     }
 }
 
